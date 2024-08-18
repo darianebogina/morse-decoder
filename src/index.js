1e7f397395cca191a348f84bdcd9acb592a95d5a
unknown <dasha.nebogina@mail.ru>
2024-08-18 14:30:40 +0300

    feat: add slice elements of array

--- src/index.js
+++ src/index.js
@@ -37,12 +37,27 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const BINARY_TABLE = {
+    '10':         '.',
+    '11':         '-',
+    '**********': ' ',
+};
+
 function decode(expr) {
     let letters = [];
+    let symbols = [];
     for (let i = 0; i < expr.length; i += 10) {
         letters.push(expr.slice(i, i + 10));
     }
-    return letters;
+    
+    for (let i = 0; i < letters.length; i++) {
+        symbols.length = 0;
+        for (let j = 0; j < 10; j += 2) {
+            symbols.push(letters[i].slice(j, j + 2));
+        }
+    }
+    
+    return symbols;
 }
 
 module.exports = {
