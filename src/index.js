81e78134956793d5039e8b7efbf52eef7a80ea27
unknown <dasha.nebogina@mail.ru>
2024-08-18 15:01:33 +0300

    feat: add morse encryption

--- src/index.js
+++ src/index.js
@@ -46,17 +46,31 @@ const BINARY_TABLE = {
 function decode(expr) {
     let letters = [];
     let symbols = [];
+    let answer = [];
     for (let i = 0; i < expr.length; i += 10) {
         letters.push(expr.slice(i, i + 10));
     }
     
     for (let i = 0; i < letters.length; i++) {
         symbols.length = 0;
+        answer.length = 0;
         for (let j = 0; j < 10; j += 2) {
             symbols.push(letters[i].slice(j, j + 2));
         }
     }
-    
+
+    for (let k = 0; k < symbols.length; k++) {
+        if (symbols[k] == 11) {
+            answer.push('-');
+        }
+        else if (symbols[k] == 10) {
+            answer.push('.');
+        }
+        else if (symbols[k] == 00) {
+            answer.push();
+        }
+    }
+
     return symbols;
 }
 
